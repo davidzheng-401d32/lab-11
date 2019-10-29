9e32b2170fde43324c35b2732652f799b1690a28
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-10-28 23:36:44 -0700

    added missing logic

---
+++ __tests__/src/auth/middleware.test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const supergoose = require('../../supergoose.js');
+const auth = require('../../../src/auth/middleware.js');
+const Users = require('../../../src/auth/users-model.js');
+
+let users = {
+  admin: {username: 'admin', password: 'password', role: 'admin'},
+  editor: {username: 'editor', password: 'password', role: 'editor'},
+  user: {username: 'user', password: 'password', role: 'user'},
+};
+
+beforeAll(async (done) => {
+  await supergoose.startDB();
+  const adminUser = await new Users(users.admin).save();
+  console.log(adminUser,editorUser,userUser);
+  const editorUser = await new Users(users.editor).save();
+  const userUser = await new Users(users.user).save();
+  done();
+});
+
+afterAll(supergoose.stopDB);
+
+xdescribe('Auth Middleware', () => {
+  
+  // admin:password: YWRtaW46cGFzc3dvcmQ=
+  // admin:foo: YWRtaW46Zm9v
+  
+  let errorObject = {'message': 'Invalid User ID/Password', 'status': 401, 'statusMessage': 'Unauthorize'};
+  
+  xdescribe('user authentication', () => {
+    
+
+    xit('fails a login for a user (admin) with the incorrect basic credentials', () => {
+
+      let cachedToken;
+      console.log(cachedToken);
+
+      let req = {
+        headers: {
+          authorization: 'Basic YWRtaW46Zm9v',
+        },
+      };
+      let res = {};
+      let next = jest.fn();
+      let middleware = auth;
+
+      return middleware(req, res, next)
+        .then(() => {
+          expect(next).toHaveBeenCalledWith(errorObject);
+        });
+
+    }); // it()
+
+    xit('logs in an admin user with the right credentials', () => {
+
+      let req = {
+        headers: {
+          authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
+        },
+      };
+      let res = {};
+      let next = jest.fn();
+      let middleware = auth;
+
+      return middleware(req,res,next)
+        .then( () => {
+          let cachedToken = req.token;
+          console.log(cachedToken);
+          expect(next).toHaveBeenCalledWith();
+        });
+
+    }); // it()
+    
+  });
+
+});
